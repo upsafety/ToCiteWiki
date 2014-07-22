dc5acb06f72769ba1f33ad29005ee93bb97dc157
upsafety <debesh.pattnaik@irissoftware.com>
2014-07-22 14:18:56 +0530

    Commit 3

---
+++ lang/en/language.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$lang = array(
+"faq" => "FAQ",
+"Add a \"Share\" link in all pages to send it via e-mail, Twitter, Facebook, message or forums" => "Add a \"Share\" link in all pages to send it via email, Twitter, Facebook, message or forums",
+"Your database requires an update to match the current Tiki version. Please proceed to <a href=\"tiki-install.php\">the installer</a>. Using Tiki with an incorrect database version usually provokes errors" => "Your database requires an update to match the the Tiki version; use <a href=\"tiki-install.php\">the installer</a>. Using Tiki with an incorrect database version will cause errors",
+"The \"Regex Wikiplugin\" is activated. It can be used by wiki editors to create any html via regex replacement" => "The \"Regex Wikiplugin\" is activated. It can be used by wiki editors to create any HTML via regex replacement",
+);
